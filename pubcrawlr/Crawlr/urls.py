497b50c1abaeb94dd33ffa1fcffa7396127984c4
iineskaroliina <2268050k@student.gla.ac.uk>
2018-03-20 15:18:44 +0000

    debugging

--- pubcrawlr/Crawlr/urls.py
+++ pubcrawlr/Crawlr/urls.py
@@ -5,7 +5,6 @@ app_name = 'Crawlr'
 urlpatterns = [
     url(r'^$', views.home, name = 'home'),
     url(r'^about/$', views.about, name = 'about'),
-    url(r'^popular/$', views.popular, name = 'popular'),
     url(r'^login/$', views.login, name = 'login'),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.show_category, name = 'show_category'),
     url(r'^register/$', views.register, name= 'register'),
