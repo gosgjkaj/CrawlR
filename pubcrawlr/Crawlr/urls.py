34d2d55e57a7e3a1fdd095f26c034cc976b31ba1
Ryan Devoil <2257271D@Student.gla.ac.uk>
2018-03-21 21:23:20 +0000

    JS now partially working in django templated, restricted line commented out of urls.py as it is not present in views at this point

--- pubcrawlr/Crawlr/urls.py
+++ pubcrawlr/Crawlr/urls.py
@@ -8,10 +8,9 @@ urlpatterns = [
     url(r'^login/$', views.login, name = 'login'),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.show_category, name = 'show_category'),
     url(r'^register/$', views.register, name= 'register'),
-    url(r'^restricted/', views.restricted, name='restricted'),
+## url(r'^restricted/', views.restricted, name='restricted'), Commented out as it's not in views.py
     url(r'^logout/$', views.user_logout, name='logout'),
 ##    url(r'^category/(?P<category_name_slug>[\w\-]+)/add_route/$', views.add_route, name='add_route'),
     url(r'^add_route/$', views.add_route, name='add_route'),
     url(r'^popular/$', views.popular, name="popular"),
-
 ]
