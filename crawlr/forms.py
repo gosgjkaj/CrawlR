8987479c90f256e51ddbb88fb296f2f30774bf88
Kara <2264457n@student.gla.ac.uk>
2018-03-28 16:22:08 +0100

    Added custom error message on save route

--- crawlr/forms.py
+++ crawlr/forms.py
@@ -14,9 +14,10 @@ class CategoryForm(forms.ModelForm):
         fields = ('name',)
 
 class RouteForm(forms.ModelForm):
+    error_messages = {'duplicate_title':'Please enter a unique name for the crawl'}
     title = forms.CharField(max_length=128,
                             help_text = "Please enter the name of the Crawl")
-   
+
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     #Hidden inputs for the variables retrieved from find directions page
     start = forms.CharField(widget=forms.HiddenInput())
@@ -27,10 +28,21 @@ class RouteForm(forms.ModelForm):
     slug = forms.CharField(widget=forms.HiddenInput(), required=False)
     created_by = forms.ModelChoiceField(queryset=User.objects.all(), widget=forms.HiddenInput())
 
-    
     class Meta:
         model = Route
         fields = ('category', 'title', 'slug', 'start', 'end', 'waypts', 'created_by')
+
+    def clean_title(self):
+        title = self.cleaned_data["title"]
+        try:
+            Route.objects.get(title=title)
+
+            raise forms.ValidationError(
+                self.error_messages['duplicate_title'], # customized error message
+                code='duplicate_title',
+            )
+        except Route.DoesNotExist:
+            return title
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder' : 'Password'}), label='')
